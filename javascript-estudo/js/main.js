0b104e37454120280f8116edb82de2fae10c66ff
abraao69 <fulllstack695@gmail.com>
2024-06-07 18:14:22 -0300

    first commit

---
+++ javascript-estudo/js/main.js
@@ -0,0 +1,112 @@
+// let idade = 18 // Tipo Number
+// let nome = "Abraão" // Tipo String
+// let logado = true // Tipo Boolean
+
+// // console.log(idade)
+// // console.log(logado)
+// // console.log(nome)
+
+// // Tipo Array e Objetos
+// // No array a contagem começa apartir do zero.
+// let lanche =["Hamburger", "Pizza", "Coca-Cola", "Salgadinho"]
+// // console.log(lanche)
+
+
+// //Objeto {}
+// let personagem = {
+//   nome: "Hulk",
+//   cor:"Verde",
+//   forca:"Maxima",
+//   contaminacao:"Raios Gama",
+//   vida:"Infinita"
+// }
+// // console.log(personagem)
+// // console.log(personagem.nome)
+
+// //Function
+
+// function multiplicar(a,b){
+//     let resultado = a * b;
+// return resultado
+// }
+// function somar(a,b){
+//     let resultado = a + b;
+// return resultado
+// }
+// function divisao(a,b){
+//     let resultado = a / b;
+// return resultado
+// }
+// function subtracao(a,b){
+//     let resultado = a - b;
+// return resultado
+// }
+// let a = somar(10, 10)
+// let b = multiplicar(10, 1000)
+// let c = subtracao(10000, 7894)
+// let d = divisao(100, 2)
+// // console.log(10*1)
+// // console.log(10*2)
+// // console.log(5*5)
+// // console.log(a)
+// // console.log(b)
+// // console.log(c)
+// // console.log(d)
+// // console.log(10+10)
+// // console.log(10/10)
+
+// //Condicional em javascript 
+
+// // if(idade>=18){
+// //     console.log("Você é maior de idade")
+// // }
+// // else{
+// //     console.log("Você é menor de idade")
+// // }
+
+
+// // if(nome == "Abraão"){
+// //     console.log("Você é Abraão")
+// // }
+// // else{
+// //     console.log("Você não e Abraão")
+// // }
+// // Tipos de condicionais
+
+// // ==
+// // >=
+// // <=
+// // <
+// // >
+// // !=
+
+// //Loop
+
+// let lista = [1 , 2, 3, 4,5]
+// // let lista2 = [ "Maçã", "Abacate" ]
+// // for(let item of lista2){
+
+// //     console.log("ingredientes  - " + item)
+// // }
+
+// let lista2 = [ "Maçã", "Abacate" ];
+// let contador = 1; // Inicia o contador a partir do número 1.
+
+// lista2.forEach((item, index) => {
+//   console.log(`Ingrediente ${contador++} - ${item}`);
+// });
+
+// for(let item of lista){
+
+//     console.log("ingredientes - " + item)
+// }
+
+// document.body.innerText = 'OLÁ MUNDO';
+
+// alert('Abraão Martins'); criar alertas
+
+// document.getElementById("texto").innerHTML="Aula <b>Javascript</b>"; insere dados no html
+ 
+// console.log("Eu sou um console.log"); imprime diretamente os dados no console.
+
+// debugger utilizado para debuggar o codigo javascript.
